9e4c3abc3e4f377dfdc33479c55147a971971a41
Noah <98993329+noah-ing@users.noreply.github.com>
2024-08-24 16:59:44 +0000

    Configure Vite for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Memory-Card-Game/", // Add your repo name here
   plugins: [react()],
   resolve: {
     alias: {
